dd2dbd48d710a3f45795afd31823c32485a79f02
Mirumo <mysteryperson520@gmail.com>
2022-08-29 23:35:00 +0800

    chore: remove debug console.log

--- routers/dataSubmit.controller.js
+++ routers/dataSubmit.controller.js
@@ -6,8 +6,6 @@ module.exports = async function (req, res) {
   for (let i = 0; i < 5; ++i) A += req.body[`A${i}`] === 'true' ? 1 : 0;
   for (let i = 0; i < 7; ++i) B += req.body[`B${i}`] === 'true' ? 1 : 0;
 
-  console.log(req.body);
-
   req.body.A = A;
   req.body.B = B;
   await newData(req.body);
