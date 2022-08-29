211e93af0a6a2c7ccda764c0a07a5a8ce809fe98
Mirumo <mysteryperson520@gmail.com>
2022-08-29 22:20:10 +0800

    chore: add new module(body-parser)

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
 const express = require('express');
+const parser = require('body-parser');
 const app = express();
 
-app.use(express.json());
+app.use(parser.json());
+app.use(parser.urlencoded());
 app.use(require('./routers'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
